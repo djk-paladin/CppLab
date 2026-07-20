733185f6dfe5069fc305e676790699ec262a27b9
djk-paladin <artemfeduck@mail.ru>
2026-07-20 10:10:09 +0300

    feat: implement Queue data structure Add Queue class supporting basic FIFO (First-In-First-Out) operations: - enqueue() - dequeue() - peek() - isEmpty() - isFull() - size()

--- Stack_Queue_List/stack_queue_list.cpp
+++ Stack_Queue_List/stack_queue_list.cpp
@@ -7,9 +7,34 @@ class Stack
     protected:
         int maxSize;
         T *array;
+        int lastPoint;
+
+        void setLastPoint(bool minus){
+
+            if(lastPoint == 0 && minus) return;
+
+            lastPoint = lastPoint > 0 && minus ? lastPoint-1 : lastPoint+1; 
+
+        }
+
+        int getLastPoint(){
+
+            return lastPoint;
+
+        }
+
+        int getMaxSize(){
+
+            return maxSize;
+
+        }
+
+        ~Stack(){
+            delete [] array;
+        }
 
     public:
-        int lastPoint;
+        
                 
     Stack(T typeData, int maxSize){
         this -> maxSize = maxSize;
@@ -17,9 +42,7 @@ class Stack
         this -> lastPoint = -1;
     }
 
-    ~Stack(){
-        delete [] array;
-    }
+    
 
     void push(T data){
 
@@ -47,44 +70,162 @@ class Stack
         
     }
 
-    void setLastPoint(bool minus){
+};
 
-        if(lastPoint == 0 && minus) return;
+template <typename T>
+class Queue
+{
 
-        lastPoint = lastPoint > 0 && minus ? lastPoint-1 : lastPoint+1; 
+    protected:
 
-    }
+        int maxSize;
+        T *array;
+        //bool firstAdd;
+        int point;
 
-    int getLastPoint(){
+        void shiftPoint(){
+            for(int i = 0; i < point - 1; i++)
+            {
+                array[i] = array[i+1];
+            }
+        }
 
-        return lastPoint;
+        void setPoint(bool next){
 
-    }
+            if(next)
+            {
+                point ++;
+            }
+            else
+            {
+                point --;
+            }
 
-    int getMaxSize(){
+        }
 
-        return maxSize;
+    public:
 
-    }
+        Queue(int size){
+            this -> maxSize = size;
+            //this -> firstAdd = true;
+            this -> point = 0;
+            this -> array = new T[maxSize];
+        }
+
+        bool isFull(){
+
+            bool checkFull = point >= maxSize;
+            return checkFull;
+
+        }
+
+        bool isEmpty(){
+
+            bool chekEmpty = point <=0;
+            return chekEmpty;
+        }
+
+        int size(){
+
+            if(!isEmpty)
+            {
+                return point-1;
+            }
+            else{
+                throw "array is empty!";
+            }
+
+        }
+
+        void enqueu(T data){
+            // if(firstAdd)
+            // {
+
+            //     array = new T[maxSize];
+
+            //     firstAdd = false;
+
+            // }
+
+            if(!isFull())
+            {
+
+                array[point] = data;
+                setPoint(true);
+
+            }
+            else{
+                throw "array is full!";
+            }
+
+        }
+
+        void dequeue(){
+
+            if(!isEmpty())
+            {
+                shiftPoint();
+                setPoint(false);
+            }
+            else
+            {
+                throw "array is empty!";
+            }
+
+        }
+
+        T peek(){
+
+            if(!isEmpty())
+            {
+                return array[0];
+            }
+            else{
+                throw "array is empty!";
+            }
+
+        }
 
 };
+
 
 // Now main is using for tests
 
 int main()
 {
 
-    // Stack stk(1, 4);
-    // //Stack arr("s", 3);
+    //test for Queue
 
-    // stk.push(1);
-    // stk.push(2);
-    // stk.push(8);
-    // stk.push(4);
+    Queue<int> qut(4);
 
-    // std::cout << stk.pop() << std::endl
-    //             << stk.pop() << std::endl
-    //             << stk.pop() << std::endl
-    //             << stk.pop() << std::endl;
+    for(int i = 1; i <= 4; i++)
+    {
+        try
+        {
+            std::cout << "is emty?" << qut.isEmpty() << std::endl;
+            qut.enqueu(i);
+            std::cout << "added " << i << std::endl;
+            std::cout << "is full?" << qut.isFull() << std::endl;
+        }
+        catch(const char* e)
+        {
+            std::cerr << e << '\n';
+        }
+    }
+
+    for(int i = 1; i <= 4; i++)
+    {
+        try
+        {
+            std::cout << "must dequed " << qut.peek() << std::endl;
+            qut.dequeue();
+        }
+        catch(const char* e)
+        {
+            std::cerr << e << '\n';
+        }
+        
+        
+    }
 
 }
